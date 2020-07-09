96810afa1e0dfba68893551b1bc3bcf4aa15fd88
SamuelEdward <samueledward40@gmail.com>
2020-07-09 12:52:22 -0700

    ,

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -7,5 +7,5 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ('author')
+    raw_id_fields = ('author',)
 
