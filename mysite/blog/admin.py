949d5bc0aaec43e6e5eb5da30df71647eeea25a0
SamuelEdward <samueledward40@gmail.com>
2020-07-09 12:48:20 -0700

    populate

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -6,4 +6,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ()
 
