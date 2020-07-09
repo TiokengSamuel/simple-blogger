97869abee7eedeb6eb5d000921f849dd045a8d02
SamuelEdward <samueledward40@gmail.com>
2020-07-09 09:11:22 -0700

    changing model setings

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,8 +9,8 @@ class Post (models.Model):
     )
 
     title = models.CharField(max_length=250)
-    slung = models.SlungField(max_length = 250, unique_for_date='publish')
-    author = models.ForeignKey(on_delete=models.CASCADE, related_name='blog_posts')
+    slung =models.SlungField(max_length = 250, unique_for_date='publish')
+    author =models.ForeignKey(on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add = True)
