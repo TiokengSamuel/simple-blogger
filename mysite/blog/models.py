9a3591ba6ced64cafd5aa2a53ae2967f79de97dc
SamuelEdward <samueledward40@gmail.com>
2020-07-09 03:59:57 -0700

    status choices

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,4 +3,7 @@ from django.utils import timezone
 form django.contrib.auth.models import User
 
 class Post (models.Model):
-    
+    STATUS_CHOICES = (
+        ('draft', 'Draft')
+        ('published', 'Published'),
+    )
