97dcad534487f1be78bad89ba343087d14455b0e
SamuelEdward <samueledward40@gmail.com>
2020-07-10 05:50:30 -0700

    postlist

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+form .models import Post
+
+def post_list(request):
+    posts = Post.published
 
 # Create your views here.
