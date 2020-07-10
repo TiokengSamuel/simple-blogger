f8049f5aee809822ba8a0c95590e3cc2b77c9399
SamuelEdward <samueledward40@gmail.com>
2020-07-10 04:43:17 -0700

    .filter

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super (PublishedManager, self).get_queryset()
+        return super (PublishedManager, self).get_queryset().filter(status='published')
 
 class Post (models.Model):
     STATUS_CHOICES = (
