9e662d1f4bad417c0738b0f4716254d9d4fd0430
SamuelEdward <samueledward40@gmail.com>
2020-07-12 00:35:30 -0700

    list view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,3 +6,8 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts' : posts})
 
 # Create your views here.
+def post_detail(request):
+    posts = get_object_or_404(Post, slug=post, status='published', publish_year=year, publish_month=month, publish_day)
+
+    return render (request, 'blog/post/detail.html', {'post': post})
+    
