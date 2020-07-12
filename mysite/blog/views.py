e0fa0d9bfc126b594ec59a4af1f1533a9a0446d5
SamuelEdward <samueledward40@gmail.com>
2020-07-11 23:31:29 -0700

    import

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, get_object_or_404
 form .models import Post
 
 def post_list(request):
-    posts = Post.published
+    posts = Post.publishedh
 
 # Create your views here.
