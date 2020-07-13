8e9d5bd986429283bc854246da08aa5747b7ffa6
SamuelEdward <samueledward40@gmail.com>
2020-07-13 12:42:42 -0700

    ending path

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     #post view
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>',
-    )
+    views.post_detail,
+    name='post_detail'),
 ]
